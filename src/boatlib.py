d97564243cea1ec1271cc53894657921f1602d03
Hayden <hcoffey1@vols.utk.edu>
2019-04-24 00:23:39 -0400

    Nice

--- src/boatlib.py
+++ src/boatlib.py
@@ -1,7 +1,4 @@
 import pins
-<<<<<<< HEAD
 
 if __name__ == "__main__":
     pass
-=======
->>>>>>> apitest
--- src/send_files_to_bots.py
+++ src/send_files_to_bots.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
 # http://recolog.blogspot.com/2013/07/transferring-files-via-bluetooth-using.html
-=======
->>>>>>> apitest
 # lightblue.sourceforge.net
 # sudo apt-get install python-bluez libbluetooth-dev python-dev
 # "                  " bluez
@@ -11,7 +8,6 @@
 
 import bluetooth
 import lightblue
-<<<<<<< HEAD
 
 def main():
     target_names = ["ACTUALLYNOTHING"] #put bluetooth names
@@ -46,35 +42,6 @@ def main():
                 print("Sent time file")
             except:
                 print("An error occured sending time file")
-=======
-import socket
-
-
-def main():
-    target_names = [] #put bluetooth names
-    file_to_send = "behavior.py"
-
-    target_address = None
-    nearby_devices = bluetooth.discover_devices()
-
-    for target in target_names:
-        for bdaddr in nearby_devices:
-            if target_name == bluetooth.lookup_name(bdaddr):
-                print("found the device")
-                target_address = bdaddr
-                break
-
-        services = lightblue.findservices(target_address)
-        for service in services:
-            if service[2] == "OBEX Object Push":
-                obex_port = service[1]
-                break
-
-        try:
-            lightblue.obex.sendfile(target_address, service[1], file_to_send)
-        except:
-            print("An error occured connecting to file")
->>>>>>> apitest
 
 if __name__ == "__main__":
     exit(main())
