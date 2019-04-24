d3c3a8934892646e05d6a804554462e79b65d668
Hayden <hcoffey1@vols.utk.edu>
2019-04-24 18:00:43 -0400

    Added LED pins

--- src/pins.py
+++ src/pins.py
@@ -1,7 +1,10 @@
 #Pins (Constant)
 
 #Outputs------
-LED = 23; #GPIO 23 (Pin 16)
+LED_FRONT   = 23; #GPIO 23 (Pin 16)
+LED_BACK    = 22; #GPIO 22 (Pin 15)
+LED_LEFT    = 27; #GPIO 27 (Pin 13)
+LED_RIGHT   = 17; #GPIO 17 (Pin 11)
 SERVO_1 = 12; #GPIO 12 (Pin 32)
 SERVO_2 = 13; #GIPO 13 (Pin 33)
 
