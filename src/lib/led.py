50dc7bea1600dd12e541138cb0aeeff8c59372b9
Hayden <hcoffey1@vols.utk.edu>
2019-04-24 19:08:34 -0400

    Updated led and gitignore

--- src/lib/led.py
+++ src/lib/led.py
@@ -8,10 +8,10 @@ class LED(Enum):
     '''
     Used to indicate which LED for usage
     '''
-    FRONT = 0
-    BACK = 1
-    LEFT = 2
-    RIGHT = 3
+    FRONT = pins.LED_FRONT
+    BACK = pins.LED_BACK
+    LEFT = pins.LED_LEFT
+    RIGHT = pins.LED_RIGHT 
 
 class STATE(Enum):
     '''
@@ -34,11 +34,4 @@ def led_toggle(led, state):
     Sets led to given state, i.e.
     led_toggle(LED.FRONT, STATE.ON) #Sets front led to on
     '''
-    if led == LED.FRONT:
-        GPIO.output(pins.LED_FRONT, value)
-    elif led == LED.BACK:
-        GPIO.output(pins.LED_BACK, value)
-    elif led == LED.LEFT:
-        GPIO.output(pins.LED_LEFT, value)
-    elif led == LED.RIGHT:
-        GPIO.output(pins.LED_RIGHT, value)
+    GPIO.output(led, state)
