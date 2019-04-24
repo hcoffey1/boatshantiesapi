99d64885cc8d3db2cfd909d63e66c4d6d44c5584
Ellis <ecritchl@vols.utk.edu>
2019-04-23 21:46:41 -0400

    send files could fail to connect/send files to bots

--- src/send_files_to_bots.py
+++ src/send_files_to_bots.py
@@ -17,6 +17,7 @@ def main():
     target_address = None
     nearby_devices = bluetooth.discover_devices()
 
+    # search might miss devices or be unable to connect, so it may need multiple attempts
     for target in target_names:
         for bdaddr in nearby_devices:
             if target_name == bluetooth.lookup_name(bdaddr):
@@ -24,20 +25,20 @@ def main():
                 target_address = bdaddr
                 break
 
-        services = lightblue.findservices(target_address)
-        for service in services:
-            if service[2] == "OBEX Object Push":
-                obex_port = service[1]
-                break
-
-        try:
-            lightblue.obex.sendfile(target_address, service[1], file_to_send)
-        except:
-            print("An error occured sending process file")
-        try:
-            lightblue.obex.sendfile(target_address, service[1], file_time)
-        except:
-            print("An error occured sending time file")
+        if target_address != None:
+            services = lightblue.findservices(target_address)
+            for service in services:
+                if service[2] == "OBEX Object Push":
+                    obex_port = service[1]
+                    break
+            try:
+                lightblue.obex.sendfile(target_address, service[1], file_to_send)
+            except:
+                print("An error occured sending process file")
+            try:
+                lightblue.obex.sendfile(target_address, service[1], file_time)
+            except:
+                print("An error occured sending time file")
 
 if __name__ == "__main__":
     exit(main())
