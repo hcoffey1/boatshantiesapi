9fbf8159af8ea385624a1af5fe576151fe9ab6d3
Hayden <hcoffey1@vols.utk.edu>
2019-04-23 22:41:22 -0400

    added ignore and update pins

--- src/pins.py
+++ src/pins.py
@@ -2,8 +2,8 @@
 
 #Outputs------
 LED = 16; #GPIO 23 (Pin 16)
-SERVO_1 = 32; #GPIO 12 (Pin 32)
-SERVO_2 = 33; #GIPO 13 (Pin 33)
+SERVO_1 = 12; #GPIO 12 (Pin 32)
+SERVO_2 = 13; #GIPO 13 (Pin 33)
 
 #Inputs------
 PHOTOTRAN = 18; #GPIO 24 (Pin 18)
