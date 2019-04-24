84775066f929175088ad13a06fad4abb86c34645
ecritchl <ecritchl@vols.utk.edu>
2019-04-24 18:38:07 -0400

    Merged several shit startup files with client

--- src/fork_kill_sim.py
+++ src/fork_kill_sim.py
@@ -1,25 +1,64 @@
 #!usr/bin/python3
-
-import time
 import multiprocessing import Process
+import bluetooth
+import socket
 import signal, psutil
+import RPi.GPIO as GPIO
+import pins
+import os
+from time import sleep
+
+print('Hello World!')
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pins.LED, GPIO.OUT, initial=GPIO.LOW)
+GPIO.output(pins.LED, GPIO.HIGH)
+sleep(1)
+GPIO.output(pins.LED, GPIO.LOW)
+
+hostMACAddress = 'whatever our mac is' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 3
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
 
 def child():
+    print("Hes going to fucking destroy me!")
+    time.wait(15)
+    print("he hasnt yet?")
+    # execfile(simulation.py)
     return
 
-def main():
-    with open('time.txt', 'r') as file:
-        time = file.readline()
-        time = int(time)
-        p = process(target = child)
-        p.start()
-        start_time = time.time()
-        # run child process and check every 10 seconds to see if it is time to end the simulation
-        while(1):
-            time.sleep(10)
-            if((time.time() - start_time) / 60 >= time):
-                parent = psutil.Process(p.pid)
-                children = parent.children(recursive=True)
-                for process in children:
-                    process.send_signal(signal.SIGTERM)
-                break
+def ALEX_JONES():
+    p = process(target = child)
+    p.start()
+    # run child process and check every 10 seconds to see if it is time to end the simulation
+    parent = psutil.Process(os.pid)
+    children = parent.children(recursive=True)
+    os.wait(5) 
+    for process in children:
+        process.send_signal(signal.SIGTERM)
+
+def main(): 
+    try:
+        client, clientInfo = s.accept()
+        with open('simulation.json', 'w+') as f:
+            while 1:
+                data = client.recv(size)
+                if data:
+                    f.write(data)
+                    print(data)
+                else:
+                    break
+        aj = process(target = ALEX_JONES)
+        aj.start()
+    except: 
+        print("Closing socket")
+        client.close()
+        s.close()
+    return
+
+
+if __name__ = "__main__":
+    main()
