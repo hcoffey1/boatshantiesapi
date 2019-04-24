4723a86306f469f9849486d8f8106bf5f43fa131
ecritchl <ecritchl@vols.utk.edu>
2019-04-24 00:22:48 -0400

    Fixed some minor issues, still may have large issues

--- src/code_to_fork_+_kill_children.py
+++ src/code_to_fork_+_kill_children.py
@@ -13,10 +13,13 @@ def main():
         time = int(time)
         p = process(target = child)
         p.start()
+        start_time = time.time()
+        # run child process and check every 10 seconds to see if it is time to end the simulation
         while(1):
-            sleep(10)
-            if(time.time() - start_time >= time):
+            time.sleep(10)
+            if((time.time() - start_time) / 60 >= time):
                 parent = psutil.Process(p.pid)
                 children = parent.children(recursive=True)
                 for process in children:
                     process.send_signal(signal.SIGTERM)
+                break
