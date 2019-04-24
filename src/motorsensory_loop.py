0cff406f0c753fb98c76fcccf7d3187797f0325f
Will Lifferth <wlifferth@icloud.com>
2019-04-24 18:51:28 -0400

    Added motorsensory_loop.py

---
+++ src/motorsensory_loop.py
@@ -0,0 +1,30 @@
+import sys
+import api
+sys.path.append("globot/globot")
+from Globot import Globot
+from Rule import Rule
+from SignalValue import SignalValue
+from SignalSet import SignalSet
+from Action import Action
+
+def get_signal_set():
+    front = SignalValue(total=api.get_front_sensor())
+    left = SignalValue(api.get_left_sensor())
+    right = SignalValue(api.get_right_sensor())
+    back = SignalValue(api.get_back_sensor())
+    return SignalSet(forward=front, right=right, left=left, back=back)
+
+def perform_action(action:Action):
+    api.turn(action.rotation)
+    api.drive(action.drive)
+    api.sleep(action.sleep)
+    api.change_signal(action.new_signal)
+
+if __name__ == "__main__":
+    path_to_instruction_set = sys.argv[1]
+    globot = Globot.build_from_file(path_to_instruction_set)
+    while True:
+        action = globot.get_action(signal_set=get_signal_set())
+        perform_action(action)
+        print(action)
+
