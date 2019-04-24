5214b73795e8d149bcece0e3041245a835d11a69
Hayden <hcoffey1@vols.utk.edu>
2019-04-24 17:13:44 -0400

    Added documentation

--- src/lib/led.py
+++ src/lib/led.py
@@ -5,50 +5,55 @@ import RPi.GPIO as GPIO
 from enum import Enum
 
 class DIRECTION(Enum):
+    '''
+    Used to indicate which LED for usage
+    '''
     FRONT = 0
     BACK = 1
     LEFT = 2
     RIGHT = 3
 
 class STATUS(Enum):
+    '''
+    Pass to functions to turn LED on/off
+    '''
     ON = GPIO.HIGH
     OFF = GPIO.LOW
 
 def led_init():
-    """
+    '''
     Initialize's led pins for usage. 
-    """
+    '''
     pass
 
 def led_front(value):
-    """
+    '''
     Write's value to front led pin
     STATUS.ON   - Turns on the LED
     STATUS.OFF  - Turns off the LED
-    """
+    '''
     pass
 
 def led_back(x):
-    """
+    '''
     Write's value to rear led pin
     STATUS.ON   - Turns on the LED
     STATUS.OFF  - Turns off the LED
-    """
+    '''
     pass
 
 def led_left(x):
-    """
+    '''
     Write's value to left led pin
     STATUS.ON   - Turns on the LED
     STATUS.OFF  - Turns off the LED
-    """
+    '''
     pass
 
 def led_right(x):
-    """
+    '''
     Write's value to right led pin
     STATUS.ON   - Turns on the LED
     STATUS.OFF  - Turns off the LED
-    """
+    '''
     pass
-
