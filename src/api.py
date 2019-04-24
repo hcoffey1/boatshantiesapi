238326e65b066878db330879958d89b11d5aa2cb
Will Lifferth <wlifferth@icloud.com>
2019-04-24 15:58:05 -0400

    Added api.py with stubs

---
+++ src/api.py
@@ -0,0 +1,24 @@
+
+def get_front_sensor():
+    return 0
+
+def get_left_sensor():
+    return 0
+
+def get_right_sensor():
+    return 0
+
+def get_back_sensor():
+    return 0
+
+def turn(angle=0):
+    pass
+
+def drive(distance=0):
+    pass
+
+def change_signal(light_intensity=0):
+    pass
+
+def sleep(sleep_time=0):
+    pass
