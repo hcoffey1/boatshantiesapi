fd511d7ebbb624199349dc72268ab250555dc0b2
ecritchl <ecritchl@vols.utk.edu>
2019-04-24 15:16:15 -0400

    changed names around

--- src/bluetooth/client.py
+++ src/bluetooth/client.py
@@ -1,19 +1,44 @@
-import bluetooth
+#!/usr/bin/env python3
+# http://recolog.blogspot.com/2013/07/transferring-files-via-bluetooth-using.html
+# lightblue.sourceforge.net
+# sudo apt-get install python-bluez libbluetooth-dev python-dev
+# "                  " bluez
+# "                  " python-bluez libbluetooth-dev python-dev
+# sudo python setup.py install
 
-hostMACAddress = 'whatever out mac is' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-port = 3
-backlog = 1
-size = 1024
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.bind((hostMACAddress, port))
-s.listen(backlog)
-try:
-    client, clientInfo = s.accept()
+import socket
+
+target_names = [["ACTUALLYNOTHING", 6969]] #put MAC addresses
+file_to_send = "behavior.py"
+file_time = "time.txt"
+
+# search might miss devices or be unable to connect, so it may need multiple attempts
+for target in target_names:
+    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s.connect((target[0], target[1]))
     while 1:
-        data = client.recv(size)
-        if data:
-            print(data)
-except: 
-    print("Closing socket")
-    client.close()
+        text = input()
+        if text == "quit":
+            break
+        s.send(bytes(text, 'UTF-8'))
+        try:
+            f = open(file_to_send, 'r')
+            file = f.read(1024)
+            while file:
+                s.send(file)
+                file = f.read(1024)
+            f.close()
+            print("Sent process file")
+        except:
+            print("An error occured sending process file")
+        try:
+            f = open(file_time, 'r')
+            file = f.read(1024)
+            while file:
+                s.send(file)
+                file = f.read(1024)
+            f.close()
+            print("Sent time file")
+        except:
+            print("An error occured sending time file")
     s.close()
--- src/bluetooth/server.py
+++ src/bluetooth/server.py
@@ -1,44 +1,19 @@
-#!/usr/bin/env python3
-# http://recolog.blogspot.com/2013/07/transferring-files-via-bluetooth-using.html
-# lightblue.sourceforge.net
-# sudo apt-get install python-bluez libbluetooth-dev python-dev
-# "                  " bluez
-# "                  " python-bluez libbluetooth-dev python-dev
-# sudo python setup.py install
+import bluetooth
 
-import socket
-
-target_names = [["ACTUALLYNOTHING", 6969]] #put MAC addresses
-file_to_send = "behavior.py"
-file_time = "time.txt"
-
-# search might miss devices or be unable to connect, so it may need multiple attempts
-for target in target_names:
-    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    s.connect((target[0], target[1]))
+hostMACAddress = 'whatever out mac is' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 3
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+try:
+    client, clientInfo = s.accept()
     while 1:
-        text = input()
-        if text == "quit":
-            break
-        s.send(bytes(text, 'UTF-8'))
-        try:
-            f = open(file_to_send, 'r')
-            file = f.read(1024)
-            while file:
-                s.send(file)
-                file = f.read(1024)
-            f.close()
-            print("Sent process file")
-        except:
-            print("An error occured sending process file")
-        try:
-            f = open(file_time, 'r')
-            file = f.read(1024)
-            while file:
-                s.send(file)
-                file = f.read(1024)
-            f.close()
-            print("Sent time file")
-        except:
-            print("An error occured sending time file")
+        data = client.recv(size)
+        if data:
+            print(data)
+except: 
+    print("Closing socket")
+    client.close()
     s.close()
