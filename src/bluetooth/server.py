30d9f2f9e590f0da9028f3c356bf8df35753953d
BeardlessBeard <thomas.beard2@yahoo.com>
2019-04-24 14:53:15 -0400

    added client and server thingies

---
+++ src/bluetooth/server.py
@@ -0,0 +1,21 @@
+import bluetooth
+
+hostMACAddress = '00:1f:e1:dd:08:3d' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 3
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+try:
+    client, clientInfo = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data) # Echo back to client
+except:
+    print("Closing socket")
+    client.close()
+    s.close()
+
