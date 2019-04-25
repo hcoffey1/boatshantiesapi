364c084eacec72be6ae7cc54e00b83e6b0fc8cbc
Hayden <hcoffey1@vols.utk.edu>
2019-04-24 21:30:18 -0400

    Updated servo

--- src/lib/servo.py
+++ src/lib/servo.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../') 
 import pins
 import RPi.GPIO as GPIO
-from enum import enum
+from enum import Enum
 from time import sleep
 
 class SERVO(Enum):
@@ -12,6 +12,10 @@ class SERVO(Enum):
     LEFT = pins.SERVO_1
     RIGHT = pins.SERVO_2
 
+class STATE(Enum):
+    CW = 1
+    CCW = 20
+
 def servo_init():
     '''
     Initialize's servo pins for usage.
@@ -19,8 +23,8 @@ def servo_init():
     GPIO.setup(pins.SERVO_1, GPIO.OUT)
     GPIO.setup(pins.SERVO_2, GPIO.OUT)
 
-def servo_toggle(servo, state):
-    p = GPIO.PWM(pins.SERVO_1, 100)
+def servo_toggle(servo, state, length):
+    p = GPIO.PWM(servo.value, 100)
     p.start(0)
-    p.ChangeDutyCycle(1)
-    sleep(1)
+    p.ChangeDutyCycle(state.value)
+    sleep(length)
