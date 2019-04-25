e892df9c5f56a33d5b80fda75a492e8d99826d9b
Hayden <hcoffey1@vols.utk.edu>
2019-04-24 23:44:38 -0400

    Updated testing script

--- src/test.py
+++ src/test.py
@@ -17,31 +17,36 @@ servo.servo_toggle(servo.SERVO.LEFT, servo.STATE.CCW)
 servo.servo_toggle(servo.SERVO.RIGHT, servo.STATE.CW)
 servo.servo_toggle(servo.SERVO.RIGHT, servo.STATE.CCW )
 
+print("FRONT LED")
 led.led_toggle(led.LED.FRONT, led.STATE.ON)
-sleep(2)
+sleep(3)
 
 print("TESTING FORWARD")
 led.led_toggle(led.LED.FRONT, led.STATE.OFF)
 motion.toggle_direction(motion.MOVE.FORWARD)
 
+print("BACK LED")
 led.led_toggle(led.LED.BACK, led.STATE.ON)
-sleep(2)
+sleep(3)
 
 print("TESTING BACK")
 led.led_toggle(led.LED.BACK, led.STATE.OFF)
 motion.toggle_direction(motion.MOVE.BACK)
 
+print("LEFT LED")
 led.led_toggle(led.LED.LEFT, led.STATE.ON)
-sleep(2)
+sleep(3)
 
 print("TESTING LEFT")
 led.led_toggle(led.LED.LEFT, led.STATE.OFF)
 motion.toggle_direction(motion.MOVE.LEFT)
+
+print("RIGHT LED")
 led.led_toggle(led.LED.RIGHT, led.STATE.ON)
-sleep(2)
+sleep(3)
 
 print("TESTING RIGHT")
 led.led_toggle(led.LED.RIGHT, led.STATE.OFF)
 motion.toggle_direction(motion.MOVE.RIGHT)
-sleep(2)
+sleep(3)
 
