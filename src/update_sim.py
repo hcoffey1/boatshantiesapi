e9ff35a21c57b317b565a2c53dd9338bfbff3d20
BeardlessBeard <thomas.beard2@yahoo.com>
2019-04-24 21:28:43 -0400

    update_sim.py: Update to update all discoverable bots

--- src/update_sim.py
+++ src/update_sim.py
@@ -2,14 +2,14 @@ import bluetooth
 import sys
 
 port = 9998
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
 devices = bluetooth.discover_devices()
+proc_list = []
 for addr in devices:
     print(addr)
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     s.connect((addr, port))
     filename = sys.argv[1]
-    with open(filename) as f:
+    with open(filename, 'r') as f:
         buf = f.read().encode('utf-8')
         s.send(buf)
     s.close()
