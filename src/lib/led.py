f13e8959e258066e2710df2a3f1276c010d42eec
Hayden <hcoffey1@vols.utk.edu>
2019-04-24 22:02:13 -0400

    Updated init handling

--- src/lib/led.py
+++ src/lib/led.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 from enum import Enum
 
 #PWM Value for LED's
-BRIGHTNESS = 30
+#BRIGHTNESS = 30
 
 class LED(Enum):
     '''
@@ -23,21 +23,26 @@ class STATE(Enum):
     ON = GPIO.HIGH
     OFF = GPIO.LOW
 
-def led_init():
-    '''
-    Initialize's led pins for usage. 
-    '''
-    GPIO.setup(pins.LED_FRONT, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pins.LED_BACK, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pins.LED_LEFT, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pins.LED_RIGHT, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pins.LED_FRONT, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pins.LED_BACK, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pins.LED_LEFT, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pins.LED_RIGHT, GPIO.OUT, initial=GPIO.LOW)
+
+#def led_init():
+#    '''
+#    Initialize's led pins for usage. 
+#    '''
+#    GPIO.setup(pins.LED_FRONT, GPIO.OUT, initial=GPIO.LOW)
+#    GPIO.setup(pins.LED_BACK, GPIO.OUT, initial=GPIO.LOW)
+    #GPIO.setup(pins.LED_LEFT, GPIO.OUT, initial=GPIO.LOW)
+    #GPIO.setup(pins.LED_RIGHT, GPIO.OUT, initial=GPIO.LOW)
 
 def led_toggle(led, state):
     '''
     Sets led to given state, i.e.
     led_toggle(LED.FRONT, STATE.ON) #Sets front led to on
     '''
-    p = GPIO.PWM(led.value, 100)
-    p.start(0)
-    p.ChangeDutyCycle(BRIGHTNESS)
-    #GPIO.output(led.value, state.value)
+    #p = GPIO.PWM(led.value, 100)
+    #p.start(0)
+    #p.ChangeDutyCycle(BRIGHTNESS)
+    GPIO.output(led.value, state.value)
--- src/lib/servo.py
+++ src/lib/servo.py
@@ -5,6 +5,9 @@ import RPi.GPIO as GPIO
 from enum import Enum
 from time import sleep
 
+GPIO.setup(pins.SERVO_1, GPIO.OUT)
+GPIO.setup(pins.SERVO_2, GPIO.OUT)
+
 class SERVO(Enum):
     '''
     Used to specify Servo
@@ -16,12 +19,12 @@ class STATE(Enum):
     CW = 1
     CCW = 20
 
-def servo_init():
-    '''
-    Initialize's servo pins for usage.
-    '''
-    GPIO.setup(pins.SERVO_1, GPIO.OUT)
-    GPIO.setup(pins.SERVO_2, GPIO.OUT)
+#def servo_init():
+#    '''
+#    Initialize's servo pins for usage.
+#    '''
+#    GPIO.setup(pins.SERVO_1, GPIO.OUT)
+#    GPIO.setup(pins.SERVO_2, GPIO.OUT)
 
 def servo_toggle(servo, state, length):
     p = GPIO.PWM(servo.value, 100)
